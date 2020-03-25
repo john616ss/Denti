05a62b7c8e215ebfc495db027587f9d1e3cb82b7
John <dead616ss@gmail.com>
2020-03-25 11:39:15 -0600

    SDK
    SDK de App Center agregado a Gradle

--- app/src/main/java/com/example/denti/MainActivity.kt
+++ app/src/main/java/com/example/denti/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.denti
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(
+            application, "Test",
+            Analytics::class.java, Crashes::class.java
+        )
 
         var btn_click_me = findViewById(R.id.btn_click_me) as Button
 
